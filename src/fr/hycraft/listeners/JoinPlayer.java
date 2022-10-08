bf21f2b82ea2a44adda6121bd10322bf9de8fb7d
BuildTools <nansou40@hotmail.fr>
2022-10-08 03:28:07 +0200

    Update JoinPlayer.class
    Update code

--- src/fr/hycraft/listeners/JoinPlayer.java
+++ src/fr/hycraft/listeners/JoinPlayer.java
@@ -22,13 +22,12 @@ public class JoinPlayer implements Listener
 		Player player = event.getPlayer();
 		Items item = new Items(player);
 		
-		if (player.getInventory().contains(item.luckyBlock()))
+		if (!player.getInventory().contains(item.luckyBlock()))
 		{
-			player.sendMessage(Rewards.PREFIX + "§cMince ! §7Vous ne pouvez pas avoir de nouveau un §6Lucky Block §7vous en possèdez déjà §cx1 §7dans votre inventaire.");
+			player.sendMessage(Rewards.PREFIX + "§cMince ! \n§7Vous ne pouvez pas avoir de nouveau un §6Lucky Block §7vous en possèdez déjà §cx1 §7dans votre inventaire.");
 			return;
 		}
 		player.getInventory().addItem(item.luckyBlock());
-		player.sendMessage(Rewards.PREFIX + "§aChouette ! §7Aujourd'hui vous avez §ax1 §6Lucky Block");
+		player.sendMessage(Rewards.PREFIX + "§aChouette ! \n§7Aujourd'hui vous avez §ax1 §6Lucky Block");
 	}
-	
 }
