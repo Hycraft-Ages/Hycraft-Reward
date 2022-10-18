bc07b10ff126a48254bcd5cfdafa424e93fbfb9a
BuildTools <nansou40@hotmail.fr>
2022-10-18 20:46:24 +0200

    Upload Maven

--- src/fr/hycraft/listeners/JoinPlayer.java
+++ src/fr/hycraft/listeners/JoinPlayer.java
@@ -1,12 +1,14 @@
 package fr.hycraft.listeners;
 
+import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
 
 import fr.hycraft.Rewards;
-import fr.hycraft.items.Items;
 
 public class JoinPlayer implements Listener
 {
@@ -15,23 +17,28 @@ public class JoinPlayer implements Listener
 	{
 		
 	}
-
+	
+	
 	@EventHandler
 	public void onJoinPlayer(PlayerJoinEvent event)
 	{
 		Player player = event.getPlayer();
-		Items item = new Items(player);
+		ItemStack luckyBlock = new ItemStack(Material.PLAYER_HEAD);
+		SkullMeta luckyMeta  = (SkullMeta) luckyBlock.getItemMeta();
 		
-		if (player.getInventory().containsAtLeast(item.luckyBlock(), 0))
-		{
-			player.getInventory().addItem(item.luckyBlock());
-			player.sendMessage(Rewards.PREFIX + "§aChouette ! \n§7Aujourd'hui vous avez §ax1 §6Lucky Block");
-		}
+		luckyMeta.setOwningPlayer(player);
+		luckyMeta.setDisplayName("§6Lucky Block");
+		luckyBlock.setItemMeta(luckyMeta);
 		
-		else if (player.getInventory().containsAtLeast(item.luckyBlock(), 1))
+		if (player.getInventory().contains(luckyBlock))
 		{
 			player.sendMessage(Rewards.PREFIX + "§cMince ! \n§7Vous ne pouvez pas avoir de nouveau un §6Lucky Block §7vous en possèdez déjà §cx1 §7dans votre inventaire.");
 			return;
+		}
+		else
+		{
+			player.getInventory().addItem(luckyBlock);
+			player.sendMessage(Rewards.PREFIX + "§aChouette ! \n§7Aujourd'hui vous avez §ax1 §6Lucky Block");
 		}
 	}
 }
